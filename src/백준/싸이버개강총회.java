0163472ac367c3ec6234c427269d8730ea27ec4f
lusate <rayrayr@naver.com>
2024-09-04 13:27:05 +0900

    [Feat] - Hash 문제
    문제 이상함 코드 실행해도 에러 발생
    문제 해결 방식은 맞게 풀었으나 compareTo(String) 방식도 가능하니 참고

--- src/백준/싸이버개강총회.java
+++ src/백준/싸이버개강총회.java
@@ -1,7 +1,40 @@
 package 백준;
 
-public class 싸이버개강총회 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
 
+public class 싸이버개강총회 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] tmp = reader.readLine().split(" ");
+        String s = tmp[0];
+        String e = tmp[1];
+        String q = tmp[2];
+        String input;
+        HashSet<String> enterMap = new HashSet<>();
+        HashSet<String> outMap = new HashSet<>();
+
+        while((input = reader.readLine()) != null){
+            String[] member = input.split(" ");
+
+            // s(개강 총회 시작 시간) - temp[0] (입장 등록 시간)을 해서 양수면 출석 완료
+            if(s.compareTo(member[0]) >= 0){
+                enterMap.add(member[1]);
+            }
+            // 퇴장 완료
+            else if(e.compareTo(member[0]) <= 0 && q.compareTo(member[0]) >= 0){
+                outMap.add(member[1]);
+            }
+        }
+
+        int answer = 0;
+        for(String str : enterMap){
+            if(outMap.contains(str)){
+                answer++;
+            }
+        }
+        System.out.println(answer);
     }
 }
