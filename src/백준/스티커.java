66c2ea4cb7bc12e01a624ce9cfb4efe3d75a03fe
lusate <rayrayr@naver.com>
2024-09-11 12:30:17 +0900

    DP

---
+++ src/백준/스티커.java
@@ -0,0 +1,51 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 스티커 {
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        while(T-->0) {
+            int N = Integer.parseInt(br.readLine());
+            int[][] arr = new int[2][N + 1];
+            int[][] dp = new int[2][N + 1];
+
+            for (int i = 0; i < 2; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 1; j <= N; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            dp[0][1] = arr[0][1];
+            dp[1][1] = arr[1][1];
+            for (int i = 2; i <= N; i++) {
+                dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + arr[0][i];
+                dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + arr[1][i];
+            }
+
+            int max = Math.max(dp[0][N], dp[1][N]);
+
+            sb.append(max).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
+
+/*
+1
+5
+50 10 100 20 40
+30 50 70 10 60
+7
+10 30 10 50 100 20 40
+20 40 30 50 60 20 80
+ */
