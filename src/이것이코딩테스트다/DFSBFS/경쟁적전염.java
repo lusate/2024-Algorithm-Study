30e0e4d8e33c5f97cfb5cb86db2430815013a4cd
lusate <rayrayr@naver.com>
2024-10-10 13:08:47 +0900

    이것이 코테다 BFS, DFS

---
+++ src/이것이코딩테스트다/DFSBFS/경쟁적전염.java
@@ -0,0 +1,92 @@
+package 이것이코딩테스트다.DFSBFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * S초가 지난 후에 (X, Y)에 존재하는 바이러스의 종류
+ */
+class Virus{
+    int x,y,virus,time;
+    Virus(int x,int y,int virus,int time){
+        this.x=x;
+        this.y=y;
+        this.virus=virus;
+        this.time=time;
+    }
+}
+public class 경쟁적전염 {
+    static int n, k;
+    static int s, row, col;
+    static int[][] map;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    static Queue<Virus> q = new LinkedList<>();
+    public static void bfs() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(map[i][j] != 0) q.add(new Virus(i,j,map[i][j],0));
+            }
+        }
+        while(!q.isEmpty()){
+            Virus tmp = q.poll();
+
+            if (tmp.time == s) {
+                return;
+            }
+
+            for (int k = 0; k < 4; k++) {
+                int nx = tmp.x + dx[k];
+                int ny = tmp.y + dy[k];
+
+                if(nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+                if (map[nx][ny] == 0) {
+                    map[nx][ny] = tmp.virus;
+                    q.add(new Virus(nx, ny, tmp.virus, tmp.time + 1));
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        map = new int[n][n];
+        ArrayList<Virus> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j] != 0) arr.add(new Virus(i, j, map[i][j], 0));
+            }
+        }
+
+        /**
+         * 리스트에 담겨있는 노드들을 바이러스가 오름차순이 되도록 정렬
+         * 정렬해준 리스트들을 모두 큐에 넣고 BFS를 사용하여 증식이 될 때마다 시간을 증가
+         * 그러면 모든 바이러스가 낮은 종류부터 동시에 처리됨.
+         * 내림차순이라면
+         * arr.sort(Comparator.comparingInt(o -> o.virus).reversed());
+         */
+        arr.sort((o1, o2) -> o1.virus - o2.virus);
+//        arr.sort(Comparator.comparingInt(o -> o.virus)); // 오름차순
+
+        for (Virus virus : arr) {
+            q.add(virus);
+        }
+
+        st = new StringTokenizer(br.readLine());
+        s = Integer.parseInt(st.nextToken());
+        row = Integer.parseInt(st.nextToken());
+        col = Integer.parseInt(st.nextToken());
+
+        bfs();
+        System.out.println(map[row - 1][col - 1]);
+    }
+}
---
+++ src/이것이코딩테스트다/DFSBFS/연구소.java
@@ -0,0 +1,98 @@
+package 이것이코딩테스트다.DFSBFS;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class 연구소 {
+    static int answer = Integer.MIN_VALUE;
+    static int n, m;
+    static int[][] map;
+    static int[][] copy;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    public static void dfs(int cnt) {
+        if (cnt == 3) {
+            // 뭔가를 함
+            bfs(); // 벽이 3개면 바이러스 퍼뜨리기 시작
+            return;
+        }
+        else {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (map[i][j] == 0) {
+                        map[i][j] = 1;
+                        dfs(cnt + 1);
+                        map[i][j] = 0;
+                    }
+                }
+            }
+        }
+    }
+    public static void bfs() {
+        Queue<int[]> q = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                copy[i][j] = map[i][j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (copy[i][j] == 2) { // 바이러스 위치
+                    q.offer(new int[]{i, j});
+                }
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int[] tmp = q.poll();
+            for (int k = 0; k < 4; k++) {
+                int nx = tmp[0] + dx[k];
+                int ny = tmp[1] + dy[k];
+
+                if(nx<0 || ny<0 || nx>=n || ny>=m) continue;
+                if (copy[nx][ny] == 0) {
+                    copy[nx][ny] = 2;
+                    q.offer(new int[]{nx, ny});
+                }
+
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(copy[i][j] == 0) cnt++;
+            }
+        }
+        answer = Math.max(answer, cnt);
+//        System.out.println("answer = " + answer);
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        copy = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dfs(0);
+        System.out.println(answer);
+    }
+}
